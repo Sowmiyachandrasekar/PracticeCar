bf40c57f2f5a2fab501252b9a0dfefe0b3d6b387
Sowmiyachandrasekar <Sowmiyachandrasekar@github.com>
2020-07-06 16:15:16 +0530

    update

--- Driver.cs
+++ Driver.cs
@@ -3,15 +3,10 @@ public class Driver
 {
     public static void Main()
     {
-        // Car ciaz=new Engine();
-        // ciaz.engine = new DieselEngine();
-        // // engine = new Engine();
-        // Console.WriteLine(engine.Start());
         Car ciaz=new Car();
         Engine engine= new DieselEngine();
         Console.WriteLine(engine.Start());
         Console.WriteLine(engine.engineType());
         Console.WriteLine(ciaz.Start());
-
     }
 }
